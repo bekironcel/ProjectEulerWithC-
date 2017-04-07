ab4bc5b8bfa0dfb8ad19b7f62a290b76f8f03b97
bekironcel <bekironcel@ogr.iu.edu.tr>
2017-04-07 17:33:43 +0300

    1

---
+++ ProjectEuler1/ProjectEuler1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam=0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if ((i%3)==0||(i%5)==0)
+                {
+                    toplam += i;
+                }
+            }
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+        }
+    }
+}
